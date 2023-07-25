66393faeb966c4c28330536b8d1cda223fb19899
Daseul1 <chlektmf4411@gmail.com>
2023-07-25 17:28:24 +0900

    [BE-03] feat: user API 작업중

--- src/apis/users/users.controller.ts
+++ src/apis/users/users.controller.ts
@@ -1,6 +1,12 @@
-import { Controller, Param } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UsersService } from './users.service';
 
 @Controller()
 export class UsersController {
-  createParent() {}
+  constructor(private usersService: UsersService) {}
+
+  createParent(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createParent(createUserDto);
+  }
 }
--- src/apis/users/users.service.ts
+++ src/apis/users/users.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  createParent(createUserDto) {
+    const { phoneNum, password } = createUserDto;
+
+    const userCheck = await this.findOneByPhoneNum();
+  }
+}
