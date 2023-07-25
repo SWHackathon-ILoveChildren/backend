43c5f0b1f45148616a1144e3e491e17b71b39137
Daseul1 <chlektmf4411@gmail.com>
2023-07-25 22:02:30 +0900

    [BE-03] feat: 비밀번호 hash 적용

--- src/apis/users/users.controller.ts
+++ src/apis/users/users.controller.ts
@@ -1,11 +1,12 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UsersService } from './users.service';
 
-@Controller()
+@Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @Post()
   createParent(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createParent(createUserDto);
   }
--- src/apis/users/users.service.ts
+++ src/apis/users/users.service.ts
@@ -6,6 +6,7 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -31,5 +32,12 @@ export class UsersService {
 
     const userCheck = await this.findOneByPhoneNum({ phoneNum });
     if (userCheck) throw new ConflictException('이미 등록된 이메일입니다.');
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    return this.usersRepository.save({
+      phoneNum,
+      password: hashedPassword,
+    });
   }
 }
