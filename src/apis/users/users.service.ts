3188ca860022cc86137c66fd0dbb61b674b79d2d
Daseul1 <chlektmf4411@gmail.com>
2023-07-25 21:47:26 +0900

    [BE-03] feat: findOneByPhoneNum 로직 추가

--- src/apis/users/users.service.ts
+++ src/apis/users/users.service.ts
@@ -1,10 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  createParent(createUserDto) {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>
+  ) {}
+
+  async findOneByPhoneNum({ phoneNum }) {
+    const user = await this.usersRepository.findOne({
+      where: { phoneNum },
+    });
+
+    if (!user) {
+      throw new UnprocessableEntityException('유저가 존재하지 않습니다.');
+    }
+
+    return user;
+  }
+
+  async createParent(createUserDto) {
     const { phoneNum, password } = createUserDto;
 
-    const userCheck = await this.findOneByPhoneNum();
+    const userCheck = await this.findOneByPhoneNum({ phoneNum });
+    if (userCheck) throw new ConflictException('이미 등록된 이메일입니다.');
   }
 }
