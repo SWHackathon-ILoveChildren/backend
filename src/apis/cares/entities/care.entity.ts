4a373338ba8bbff71a25b82346f0d9d8aa2d9c15
Daseul1 <chlektmf4411@gmail.com>
2023-07-28 16:08:27 +0900

    [BE-16] Docs: care entity 수정 및 스웨거 추가

--- src/apis/cares/entities/care.entity.ts
+++ src/apis/cares/entities/care.entity.ts
@@ -1,21 +1,80 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsEnum, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { Children } from 'src/apis/children/entities/children.entity';
-import { Profile } from 'src/apis/profiles/entities/profile.entity';
-import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { STATUS_TYPE_ENUM } from 'src/apis/cares/types/status.type';
+import { User } from 'src/apis/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  CreateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Care {
+  @ApiProperty({
+    example: '0dc011aa-d76e-11ed-afa1-0242ac120002',
+    required: true,
+  })
+  @IsUUID()
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: '2023-07-28',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
   @Column()
-  startDate: string;
+  date: string;
 
+  @ApiProperty({
+    example: '09:00',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
   @Column()
-  endDate: string;
+  startTime: string;
 
+  @ApiProperty({
+    example: '18:00',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Column()
+  endTime: string;
+
+  @ApiProperty({
+    example: 'SCHEDULE',
+    type: 'enum',
+    required: true,
+  })
+  @IsEnum(STATUS_TYPE_ENUM)
+  @IsNotEmpty()
+  @Column({ type: 'enum', enum: STATUS_TYPE_ENUM })
+  careStatus: STATUS_TYPE_ENUM;
+
+  @ApiProperty({
+    example: '2023-07-28T06:48:36.266Z',
+  })
+  @IsDate()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ApiProperty({
+    type: () => Children,
+    example: 'children: { id: 0dc011aa-d76e-11ed-afa1-0242ac120002 }',
+  })
   @ManyToOne(() => Children)
   children: Children;
 
-  @ManyToOne(() => Profile)
-  profile: Profile;
+  @ApiProperty({
+    example: 'user: { id: 0dc011aa-d76e-11ed-afa1-0242ac120002 }',
+  })
+  @ManyToOne(() => User)
+  user: User;
 }
