d91ede5574de84a4077118a5a010734d89d1ffeb
Daseul1 <chlektmf4411@gmail.com>
2023-07-31 00:33:54 +0900

    [BE-28] Feat: childType create API 작업중

--- src/apis/childTypes/childType.controller.ts
+++ src/apis/childTypes/childType.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ChildTypeService } from './childType.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ChildType } from './entities/childType.entity';
+import { CreateChildTypeDto } from './dto/createChildType.dto';
 
-@Controller()
-export class ChildTypeController {}
+@Controller('childType')
+export class ChildTypeController {
+  constructor(private childTypeService: ChildTypeService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: '아이 타입 생성 API (데이터베이스 생성용)',
+  })
+  @ApiResponse({
+    status: 201,
+    description: '생성 성공',
+    type: ChildType,
+  })
+  @ApiResponse({
+    status: 409,
+    description: '생성 실패',
+  })
+  createChildType(@Body() createChildTypeDto: CreateChildTypeDto) {
+    return this.childTypeService.create({ createChildTypeDto });
+  }
+}
--- src/apis/childTypes/childType.service.ts
+++ src/apis/childTypes/childType.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ChildType } from './entities/childType.entity';
+import { Repository } from 'typeorm';
+import { CreateChildTypeDto } from './dto/createChildType.dto';
 
 @Injectable()
-export class ChildTypeService {}
+export class ChildTypeService {
+  constructor(
+    @InjectRepository(ChildType)
+    private childTypeRepository: Repository<ChildType>
+  ) {}
+
+  async create({ createChildTypeDto }: CreateChildTypeDto) {
+    // await this.findOne/
+  }
+}
