fcc139441d80b0ce8a1c95ad783e73fd541b7788
Daseul1 <chlektmf4411@gmail.com>
2023-07-30 16:07:11 +0900

    Feat: 로그인 API 서비스 작업중

--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -1,6 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  loginOAuth(loginDto) {}
+  constructor(private usersService: UsersService) {}
+
+  async loginOAuth({ loginDto, req, res }) {
+    const { phoneNum, password } = loginDto;
+
+    const user = await this.usersService.findOneByPhoneNum({ phoneNum });
+
+    if (!user)
+      throw new UnprocessableEntityException(
+        '존재하지 않는 회원 정보입니다. 회원가입을 진행해 주세요.'
+      );
+
+    const isAuth = await bcrypt.compare(password, user.password);
+    if (!isAuth)
+      throw new UnprocessableEntityException(
+        '비밀번호가 틀렸습니다. 다시 시도해주세요.'
+      );
+
+    // 3. 회원가입 O -> 로그인
+
+    // 4. 리다이렉트
+  }
 }
