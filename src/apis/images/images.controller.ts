316c03dbc6b02d16a3110f0b1a747bd2bc4b092c
Daseul1 <chlektmf4411@gmail.com>
2023-08-02 22:26:28 +0900

    [BE-50] Feat: 컨트롤러 작성

--- src/apis/images/images.controller.ts
+++ src/apis/images/images.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { ImagesService } from './images.service';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { SaveImagesDto } from './dto/saveImages.dto';
 
 @Controller('images')
-export class ImagesController {}
+export class ImagesController {
+  constructor(private imagesService: ImagesService) {}
+
+  @Post(':sitterUserId')
+  @ApiOperation({
+    summary: '업로드 된 이미지 저장 API',
+    description: 'GCP Bucket에 업로드 된 이미지 URL을 DB에 저장하기 위한 API',
+  })
+  @ApiResponse({
+    status: 201,
+    description: '저장 성공',
+    // type:
+  })
+  @ApiResponse({
+    status: 422,
+    description: '저장 실패',
+  })
+  saveImages(
+    @Param('sitterUserId') sitterUserId: string,
+    @Body() saveImagesDto: SaveImagesDto
+  ) {
+    return this.imagesService.save({ sitterUserId, saveImagesDto });
+  }
+}
