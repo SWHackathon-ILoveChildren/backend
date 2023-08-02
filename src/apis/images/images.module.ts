cb4228ff4dd2965531f18efcfe606090e00006e2
Daseul1 <chlektmf4411@gmail.com>
2023-08-02 22:31:17 +0900

    [BE-50] Feat: 서비스 작업중

--- src/apis/images/images.module.ts
+++ src/apis/images/images.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Image } from './entities/image.entity';
 import { ImagesService } from './images.service';
 import { ImagesController } from './images.controller';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Image])],
+  imports: [TypeOrmModule.forFeature([Image]), UsersModule],
   providers: [ImagesService],
   controllers: [ImagesController],
 })
--- src/apis/images/images.service.ts
+++ src/apis/images/images.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Image } from './entities/image.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class ImagesService {}
+export class ImagesService {
+  constructor(
+    @InjectRepository(Image)
+    private imagesRepository: Repository<Image>,
+
+    private usersService: UsersService
+  ) {}
+
+  async save({ sitterUserId, saveImagesDto }) {
+    const user = this.usersService.sitterUserFindOneById({ sitterUserId });
+
+    const result = await this.imagesRepository.save({
+      url: saveImagesDto,
+    });
+
+    console.log(result);
+  }
+}
