4204bc2f1f52a210106aff3141ff711350164e67
Daseul1 <chlektmf4411@gmail.com>
2023-08-02 22:35:29 +0900

    [BE-50] Docs: 스웨거 문서 작성

--- src/apis/images/entities/image.entity.ts
+++ src/apis/images/entities/image.entity.ts
@@ -1,11 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUUID } from 'class-validator';
 import { User } from 'src/apis/users/entities/user.entity';
 import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 
 @Entity()
 export class Image {
+  @ApiProperty({
+    example: '0dc011aa-d76e-11ed-afa1-0242ac120002',
+  })
+  @IsUUID()
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example:
+      'https://storage.googleapis.com/karuru-storage/2023-08-02/5b677e6d-5cf0-49f1-bb6a-c42db0e62e35/3.png',
+  })
+  @IsString()
   @Column()
   url: string;
 
