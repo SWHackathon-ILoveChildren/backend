d171f279f67a41b9c9116cd4b3e7c75c99f5a991
Daseul1 <chlektmf4411@gmail.com>
2023-08-02 17:53:04 +0900

    [BE-42] Feat: 업로드 API 컨드롤러 작성

--- src/apis/files/files.controller.ts
+++ src/apis/files/files.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { ApiConsumes, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('files')
-export class FilesController {}
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post()
+  @ApiConsumes('multipart/form-data')
+  @ApiOperation({
+    summary: '시니어시터 수료증 이미지 업로드 API',
+    description:
+      'field key는 "file", 갯수 제한 1개, 업로드된 이미지는 GCP BUCKET 내 존재',
+  })
+  @ApiResponse({
+    status: 201,
+    description: '업로드 성공',
+    // type:
+  })
+  @ApiResponse({
+    status: 422,
+    description: '업로드 실패',
+  })
+  @UseInterceptors(FilesInterceptor('file', 1))
+  async uploadFiles(
+    @UploadedFile()
+    file: Express.Multer.File
+  ) {
+    // return await this.filesService.uploadImages(file);
+  }
+}
